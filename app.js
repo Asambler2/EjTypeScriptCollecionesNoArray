0d07a08fb0415584bf7383ca725b2a624ce551f3
Asambler2 <chema508@hotmail.com>
2024-04-16 10:48:39 +0200

    Ej 8 casi terminado

--- app.js
+++ app.js
@@ -31,4 +31,73 @@ function enviarClickado() {
 }
 if (enviar != null)
     enviar.addEventListener('click', enviarClickado, false);
+/*Ejercicio 8 parte del Set:*/
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
+let nombreSet = document.getElementById('nombrePersona');
+let edadSet = document.getElementById('edadPersona');
+let enviarPersona = document.getElementById('crearPersona');
+let mostrarMedia = document.getElementById('edadMedia');
+let setPersonas = new Set();
+if (mostrarMedia != null)
+    mostrarMedia.innerHTML = 'Edad media = 0';
+function crearPersona() {
+    let persona = new Persona(nombreSet.value, Number(edadSet.value));
+    let sumaEdades = 0;
+    if (setPersonas.has(persona)) {
+        alert('Persona duplicada');
+    }
+    else {
+        setPersonas.add(persona);
+    }
+    setPersonas.forEach((personas) => sumaEdades += personas.edad);
+    if (mostrarMedia != null)
+        mostrarMedia.innerHTML = `Persona creada: nombre: ${persona.nombre}, edad: ${persona.edad}, edad media personas creadas: ${Math.ceil(sumaEdades / setPersonas.size)}`;
+}
+if (enviarPersona != null)
+    enviarPersona.addEventListener('click', crearPersona, false);
+class Empleado extends Persona {
+    constructor(nombre, edad, id, exp) {
+        super(nombre, edad);
+        this.id = id;
+        this.exp = exp;
+    }
+}
+let nombreMap = document.getElementById('nombreEmpleado');
+let edadMap = document.getElementById('edadEmpleado');
+let idMap = document.getElementById('idEmpleado');
+let expMap = document.getElementById('expEmpleado');
+let enviarEmpleado = document.getElementById('crearEmpleado');
+let mostrar = document.getElementById('mostrar');
+let empleadosMap = new Map();
+if (mostrar != null)
+    mostrar.innerHTML = 'Edad media = 0, experiencia acumulada = 0';
+function crearEmpleado() {
+    let empleado = new Empleado(nombreMap.value, Number(edadMap.value), idMap.value, Number(expMap.value));
+    if (empleadosMap.has(empleado.id)) {
+        alert('Empleado duplicada');
+    }
+    else {
+        empleadosMap.set(empleado.id, empleado);
+    }
+    mostrarEmpleado(empleado);
+}
+function mostrarEmpleado(empleado) {
+    let sumaEdades = 0;
+    let expTotal = 0;
+    empleadosMap.forEach(function (empleado1, key) {
+        sumaEdades += empleado.edad;
+        expTotal += empleado.exp;
+    });
+    if (mostrar != null)
+        mostrar.innerHTML = `Empleado creada: nombre: ${empleado.nombre}, edad: ${empleado.edad}, edad media personas empleados: ${Math.ceil(sumaEdades / empleadosMap.size)}`;
+    if (mostrar != null)
+        mostrar.innerHTML += '\n El id del empleado = ' + empleado.id + ' y la experienci es = ' + empleado.exp + ' la experiencia total es = ' + expTotal;
+}
+if (enviarEmpleado != null)
+    enviarEmpleado.addEventListener('click', crearEmpleado, false);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -28,4 +28,85 @@ function enviarClickado() {
     }
 }
 
-if(enviar != null)enviar.addEventListener('click', enviarClickado, false);
+if (enviar != null) enviar.addEventListener('click', enviarClickado, false);
+
+
+/*Ejercicio 8 parte del Set:*/
+
+class Persona {
+    nombre: string;
+    edad: number;
+
+    constructor(nombre: string, edad: number) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
+
+let nombreSet = <HTMLInputElement>document.getElementById('nombrePersona');
+let edadSet = <HTMLInputElement>document.getElementById('edadPersona');
+let enviarPersona = document.getElementById('crearPersona');
+let mostrarMedia = document.getElementById('edadMedia');
+
+let setPersonas = new Set<Persona>();
+
+if(mostrarMedia != null)mostrarMedia.innerHTML = 'Edad media = 0';
+
+function crearPersona() {
+    let persona: Persona = new Persona(nombreSet.value, Number(edadSet.value));
+    let sumaEdades = 0;
+    if (setPersonas.has(persona)) {
+        alert('Persona duplicada');
+    } else {
+        setPersonas.add(persona);
+    }
+    setPersonas.forEach((personas: Persona) => sumaEdades += personas.edad);
+    if (mostrarMedia != null) mostrarMedia.innerHTML = `Persona creada: nombre: ${persona.nombre}, edad: ${persona.edad}, edad media personas creadas: ${Math.ceil(sumaEdades / setPersonas.size)}`;
+}
+
+if (enviarPersona != null) enviarPersona.addEventListener('click', crearPersona, false);
+
+class Empleado extends Persona {
+    id: string;
+    exp: number;
+
+    constructor(nombre: string, edad: number, id: string, exp: number) {
+        super(nombre, edad);
+        this.id = id;
+        this.exp = exp;
+    }
+}
+
+let nombreMap = <HTMLInputElement>document.getElementById('nombreEmpleado');
+let edadMap = <HTMLInputElement>document.getElementById('edadEmpleado');
+let idMap = <HTMLInputElement>document.getElementById('idEmpleado');
+let expMap = <HTMLInputElement>document.getElementById('expEmpleado');
+let enviarEmpleado = document.getElementById('crearEmpleado');
+let mostrar = document.getElementById('mostrar');
+
+let empleadosMap = new Map<string, Empleado>();
+
+if (mostrar!= null) mostrar.innerHTML = 'Edad media = 0, experiencia acumulada = 0';
+
+function crearEmpleado() {
+    let empleado: Empleado = new Empleado(nombreMap.value, Number(edadMap.value), idMap.value, Number(expMap.value));
+    if (empleadosMap.has(empleado.id)) {
+        alert('Empleado duplicada');
+    } else {
+        empleadosMap.set(empleado.id, empleado);
+    }
+    mostrarEmpleado(empleado);
+}
+
+function mostrarEmpleado(empleado : Empleado) {
+    let sumaEdades = 0;
+    let expTotal = 0;
+    empleadosMap.forEach(function (empleado1: Empleado, key: string) {
+        sumaEdades += empleado.edad;
+        expTotal += empleado.exp;
+    });
+    if (mostrar != null) mostrar.innerHTML = `Empleado creada: nombre: ${empleado.nombre}, edad: ${empleado.edad}, edad media personas empleados: ${Math.ceil(sumaEdades / empleadosMap.size)}`;
+    if (mostrar != null) mostrar.innerHTML += '\n El id del empleado = ' + empleado.id + ' y la experienci es = ' + empleado.exp + ' la experiencia total es = ' + expTotal;
+}
+
+if (enviarEmpleado != null) enviarEmpleado.addEventListener('click', crearEmpleado, false);
